ef8e5a965a4bb4310661b3e3dd1c676929e674df
mzhub-lab <yogedrasama@gmail.com>
2026-07-07 10:16:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -173,7 +173,7 @@ if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "naval-klarika-vlzbots-88759354.koyeb.app/")
+URL = environ.get("FQDN", "")
 
 # Commands
 admin_cmds = [
